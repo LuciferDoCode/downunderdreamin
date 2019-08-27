fdabd6e23ebd281abdcef5fc3d250e347c744237
Tarique Shamim <tarique@concret.io>
2019-08-28 03:33:42 +0530

    updated

--- force-app/main/default/lwc/lwcHelloWorld/lwcHelloWorld.js
+++ force-app/main/default/lwc/lwcHelloWorld/lwcHelloWorld.js
@@ -1,3 +1,15 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class LwcHelloWorld extends LightningElement {}
+export default class LwcHelloWorld extends LightningElement {
+    @track welcomeMsg = 'Coming from India.'
+    genericChangeHandler(event) {
+        const field = event.target.name
+        switch (field) {
+            case 'welcome-msg':
+                this.welcomeMsg = event.target.value
+                break
+            default:
+                break
+        }
+    }
+}
